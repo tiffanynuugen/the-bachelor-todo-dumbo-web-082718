f19a6fcd1281e16e8eeb8a3877af8cf355c60fd9
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 21:10:01 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,18 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+       people.each do |k,v|
+         binding.pry
+        if v == "Winner"
+          full_name = people["name"]
+          return full_name.split(' ').first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
 end
 
 def count_contestants_by_hometown(data, hometown)
