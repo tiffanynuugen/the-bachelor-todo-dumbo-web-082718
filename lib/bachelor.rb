e578a5036e809686efaedb566cdba30fbab2c46d
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 22:29:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |info|
+  data.each do |info|
        info.each do |k,v|
          binding.pry
         if v == "Winner"
