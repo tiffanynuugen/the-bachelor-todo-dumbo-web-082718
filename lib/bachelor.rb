81ebb61690494bf8eb30e89edcb3a0549cea8269
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 23:20:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,9 +38,28 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
-end
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
+ end
 
-def get_average_age_for_season(data, season)
-  # code here
+def get_average_age_for_season(data, season)	
+ age = 0
+  count = 0
+   data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+         age += v.to_f
+      end
+    end
+  end
+   answer = (age/count).round
+return answer
 end
